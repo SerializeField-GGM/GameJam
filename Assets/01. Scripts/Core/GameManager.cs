4322f6271d2c70ad7880550c72890a9ce8fac356
성지은 <sungjieun.dev@gmail.com>
2022-07-13 12:53:47 +0900

    3

---
+++ Assets/01. Scripts/Core/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance = null;
+        [SerializeField] List<PoolableMono> poolingList = new List<PoolableMono>();
+        private Transform pooler = null;
+        private void Awake()
+        {
+            if(Instance == null) Instance = this;
+
+            PoolManager.Instance = new PoolManager();
+
+            pooler = transform.GetChild(0);
+            if(pooler == null) { return; }
+            foreach(PoolableMono prefab in poolingList)
+                PoolManager.Instance.CreatePool(prefab, pooler);
+        }
+    }
+}
+
